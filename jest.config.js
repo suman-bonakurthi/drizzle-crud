a2f2a5ec67054cdc9b2ba854e9acf696b37c9774
Suman Bonakurthi <sumanbonakurthi@gmail.com>
2025-10-18 12:14:53 +0700

    updated test files

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,js}',
+    '!src/**/*.d.ts',
+    '!src/index.ts' // Exclude the index file as it's just exports
+  ],
+ coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'clover'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.json',
+      },
+    ],
+  },
+};
